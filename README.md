8c086291c52baa26709554c97eb31700ec6ccbd8
Zynith0 <nolan.lessard.music@gmail.com>
2025-09-19 12:55:41 -0400

    FEAT: added init-src, build-src, and run-src

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ A cli tool to add missing features from go
 init [project name]: initializes a project with the name being [project name]  
 run: runs cmd/main.go  
 build: builds cmd/main.go  
+init-src [project name]: initializes a project with the name being [project name] using src instead of cmd  
+run-src: runs src/main.go  
+build-src: builds src/main.go  
--- src/main.go
+++ src/main.go
@@ -18,8 +18,7 @@ func main() {
     if len(flag.Args()) < 1 {
         fmt.Println("The current commands are:")
         fmt.Println("init       initializes a go project")
-		// TODO: actually make init-src a thing
-        // fmt.Println("init-src   initializes a go project using src instead of cmd")
+        fmt.Println("init-src   initializes a go project using src instead of cmd")
         fmt.Println("run        runs the project")
         fmt.Println("build      builds the project")
     }
@@ -36,6 +35,14 @@ func main() {
 		run()
 	} else if flag.Arg(0) == "build" {
 		build()
+	} else if flag.Arg(0) == "init-src" {
+		var projectName = flag.Arg(1)
+
+		initProjectSrc(projectName)
+    } else if flag.Arg(0) == "run-src" {
+		runSrc()
+	} else if flag.Arg(0) == "build-src" {
+		buildSrc()
 	}
 }
 
@@ -103,6 +110,82 @@ func build() {
 	}
 
 	cmd := exec.Command("go", "build", "cmd/main.go")
+	if err != nil {
+		fmt.Println("skill issue", err)
+	}
+	cmd.Dir = dir
+
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("skill issue", err)
+	}
+	fmt.Println(string(output))
+}
+
+func initProjectSrc(projectName string) {
+    os.Mkdir(projectName, 0755)
+    os.Mkdir(projectName + "/src", 0755)
+	mainGo, err := os.Create(projectName  + "/src/" + "main.go")
+
+	mainGo.WriteString("package main\n\nimport (\n	" + `"` + "fmt" + `"` + "\n)\n\nfunc main() {\n	fmt.Println(" + `"` + "Hello, World!" + `"` + ")\n}")
+
+	userName, err := exec.Command("git", "config", "--global", "user.name").Output()
+	if err != nil {
+		fmt.Println("skill issue", err)
+	}
+
+	userNameOut := string(userName)
+
+    cmd := exec.Command("go", "mod", "init", "github.com/" + strings.Trim(userNameOut, "\n") + "/" +  projectName)
+    cmd.Dir = "./" + projectName
+	output, err := cmd.Output()
+    if err != nil {
+        fmt.Println("skill issue", err)
+    }
+    fmt.Println(string(output))
+
+    gitInit := exec.Command("git", "init")
+    gitInit.Dir = "./" + projectName
+    gitInitOutput, err := gitInit.Output()
+
+    if err != nil {
+        fmt.Println("skill issue", err)
+    }
+    fmt.Println(string(gitInitOutput))
+
+	f, err := os.Create(projectName + "/.gitignore")
+	if err != nil {
+		fmt.Println("skill issue", err)
+	}
+	f.WriteString("main\n*.exe")
+}
+
+func runSrc() {
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("skill issue")
+	}
+
+	cmd := exec.Command("go", "run", "src/main.go")
+	if err != nil {
+		fmt.Println("skill issue", err)
+	}
+	cmd.Dir = dir
+
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("skill issue", err)
+	}
+	fmt.Println(string(output))
+}
+
+func buildSrc() {
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("skill issue")
+	}
+
+	cmd := exec.Command("go", "build", "src/main.go")
 	if err != nil {
 		fmt.Println("skill issue", err)
 	}
