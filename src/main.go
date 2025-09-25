a4b532079184228e0a430928fd24fab837eb588d
Zynith0 <nolan.lessard.music@gmail.com>
2025-09-25 17:17:21 -0400

    FEAT: init-web

--- src/main.go
+++ src/main.go
@@ -9,29 +9,29 @@ import (
 )
 
 func main() {
-    var init string
-    flag.StringVar(&init, "Project Name", "", "The name of the project to be made")
+	var init string
+	flag.StringVar(&init, "Project Name", "", "The name of the project to be made")
 
-    flag.Parse()
+	flag.Parse()
 
 	// Print a help message if the user doesn't input any flags
-    if len(flag.Args()) < 1 {
-        fmt.Println("The current commands are:")
-        fmt.Println("init       initializes a go project")
-        fmt.Println("init-src   initializes a go project using src instead of cmd")
-        fmt.Println("run        runs the project")
-        fmt.Println("build      builds the project")
-    }
+	if len(flag.Args()) < 1 {
+		fmt.Println("The current commands are:")
+		fmt.Println("init       initializes a go project")
+		fmt.Println("init-src   initializes a go project using src instead of cmd")
+		fmt.Println("run        runs the project")
+		fmt.Println("build      builds the project")
+	}
 
-    if flag.Arg(0) == "init" {
-        if len(flag.Args()) < 2 {
-            fmt.Println("You need to specify a project name!")
-        } else {
-            var projectName = flag.Arg(1)
+	if flag.Arg(0) == "init" {
+		if len(flag.Args()) < 2 {
+			fmt.Println("You need to specify a project name!")
+		} else {
+			var projectName = flag.Arg(1)
 
-            initProject(projectName)
-        }
-    } else if flag.Arg(0) == "run" {
+			initProject(projectName)
+		}
+	} else if flag.Arg(0) == "run" {
 		run()
 	} else if flag.Arg(0) == "build" {
 		build()
@@ -39,16 +39,24 @@ func main() {
 		var projectName = flag.Arg(1)
 
 		initProjectSrc(projectName)
-    } else if flag.Arg(0) == "run-src" {
+	} else if flag.Arg(0) == "run-src" {
 		runSrc()
 	} else if flag.Arg(0) == "build-src" {
 		buildSrc()
+	} else if flag.Arg(0) == "init-web" {
+		if len(flag.Args()) < 2 {
+			fmt.Println("You need to specify a project name!")
+		} else {
+			var projectName = flag.Arg(1)
+
+			initWebProject(projectName)
+		}
 	}
 }
 
 func initProject(projectName string) {
-    os.Mkdir(projectName, 0755)
-    os.Mkdir(projectName + "/cmd", 0755)
+	os.Mkdir(projectName, 0755)
+	os.Mkdir(projectName + "/cmd", 0755)
 	mainGo, err := os.Create(projectName  + "/cmd/" + "main.go")
 
 	mainGo.WriteString("package main\n\nimport (\n	" + `"` + "fmt" + `"` + "\n)\n\nfunc main() {\n	fmt.Println(" + `"` + "Hello, World!" + `"` + ")\n}")
@@ -60,22 +68,22 @@ func initProject(projectName string) {
 
 	userNameOut := string(userName)
 
-    cmd := exec.Command("go", "mod", "init", "github.com/" + strings.Trim(userNameOut, "\n") + "/" +  projectName)
-    cmd.Dir = "./" + projectName
+	cmd := exec.Command("go", "mod", "init", "github.com/" + strings.Trim(userNameOut, "\n") + "/" +  projectName)
+	cmd.Dir = "./" + projectName
 	output, err := cmd.Output()
-    if err != nil {
-        fmt.Println("skill issue", err)
-    }
-    fmt.Println(string(output))
+	if err != nil {
+		fmt.Println("skill issue", err)
+	}
+	fmt.Println(string(output))
 
-    gitInit := exec.Command("git", "init")
-    gitInit.Dir = "./" + projectName
-    gitInitOutput, err := gitInit.Output()
+	gitInit := exec.Command("git", "init")
+	gitInit.Dir = "./" + projectName
+	gitInitOutput, err := gitInit.Output()
 
-    if err != nil {
-        fmt.Println("skill issue", err)
-    }
-    fmt.Println(string(gitInitOutput))
+	if err != nil {
+		fmt.Println("skill issue", err)
+	}
+	fmt.Println(string(gitInitOutput))
 
 	f, err := os.Create(projectName + "/.gitignore")
 	if err != nil {
@@ -123,8 +131,8 @@ func build() {
 }
 
 func initProjectSrc(projectName string) {
-    os.Mkdir(projectName, 0755)
-    os.Mkdir(projectName + "/src", 0755)
+	os.Mkdir(projectName, 0755)
+	os.Mkdir(projectName + "/src", 0755)
 	mainGo, err := os.Create(projectName  + "/src/" + "main.go")
 
 	mainGo.WriteString("package main\n\nimport (\n	" + `"` + "fmt" + `"` + "\n)\n\nfunc main() {\n	fmt.Println(" + `"` + "Hello, World!" + `"` + ")\n}")
@@ -136,22 +144,22 @@ func initProjectSrc(projectName string) {
 
 	userNameOut := string(userName)
 
-    cmd := exec.Command("go", "mod", "init", "github.com/" + strings.Trim(userNameOut, "\n") + "/" +  projectName)
-    cmd.Dir = "./" + projectName
+	cmd := exec.Command("go", "mod", "init", "github.com/" + strings.Trim(userNameOut, "\n") + "/" +  projectName)
+	cmd.Dir = "./" + projectName
 	output, err := cmd.Output()
-    if err != nil {
-        fmt.Println("skill issue", err)
-    }
-    fmt.Println(string(output))
+	if err != nil {
+		fmt.Println("skill issue", err)
+	}
+	fmt.Println(string(output))
 
-    gitInit := exec.Command("git", "init")
-    gitInit.Dir = "./" + projectName
-    gitInitOutput, err := gitInit.Output()
+	gitInit := exec.Command("git", "init")
+	gitInit.Dir = "./" + projectName
+	gitInitOutput, err := gitInit.Output()
 
-    if err != nil {
-        fmt.Println("skill issue", err)
-    }
-    fmt.Println(string(gitInitOutput))
+	if err != nil {
+		fmt.Println("skill issue", err)
+	}
+	fmt.Println(string(gitInitOutput))
 
 	f, err := os.Create(projectName + "/.gitignore")
 	if err != nil {
@@ -196,4 +204,50 @@ func buildSrc() {
 		fmt.Println("skill issue", err)
 	}
 	fmt.Println(string(output))
+}
+
+func initWebProject(projectName string) {
+	os.Mkdir(projectName, 0755)
+	os.Mkdir(projectName + "/cmd", 0755)
+	os.Mkdir(projectName + "/internal", 0755)
+	os.Mkdir(projectName + "/internal/handler", 0755)
+	os.Mkdir(projectName + "/view", 0755)
+	mainGo, err := os.Create(projectName  + "/cmd/" + "main.go")
+	handlerGo, err := os.Create(projectName  + "/internal/handler/" + "handler.go")
+	_, err = os.Create(projectName  + "/view/" + "index.html")
+
+	userName, err := exec.Command("git", "config", "--global", "user.name").Output()
+	if err != nil {
+		fmt.Println("skill issue", err)
+	}
+
+	userNameOut := string(userName)
+
+	mainGo.WriteString("package main\n\nimport (\n	" + `"` + "net/http" + `"` + "\n	" + `"` + "github.com/" + strings.Trim(userNameOut, "\n") + "/" + projectName + "/internal/handler" + `"` + "\n)\n\nfunc main() {\n	mux := http.NewServeMux()\n	mux.HandleFunc(" + `"` + "/" + `"` + ", handler.HandleRoot)\n\n	http.ListenAndServe(" + `"` + ":8080" + `"` + ", mux)\n}")
+
+	handlerGo.WriteString("package handler\n\nimport (\n	" + `"` + "net/http" + `"` + "\n	" + `"` + "html/template" + `"` + "\n	" + `"` + "fmt" + `"` + "\n)\n\nfunc HandleRoot(w http.ResponseWriter, r *http.Request) {\n	renderTemplate(" + `"` + "view/index.html" + `"` + ", w" + ")\n}\n\nfunc renderTemplate(tmpl string, w http.ResponseWriter) {\n	t, err := template.ParseFiles(tmpl)\n	if err != nil {\n		fmt.Println(" + `"` + "skill issue" + `"` + ", err)\n	}\n	t.Execute(w, " + `""` + ")\n}")
+
+
+	cmd := exec.Command("go", "mod", "init", "github.com/" + strings.Trim(userNameOut, "\n") + "/" +  projectName)
+	cmd.Dir = "./" + projectName
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("skill issue", err)
+	}
+	fmt.Println(string(output))
+
+	gitInit := exec.Command("git", "init")
+	gitInit.Dir = "./" + projectName
+	gitInitOutput, err := gitInit.Output()
+
+	if err != nil {
+		fmt.Println("skill issue", err)
+	}
+	fmt.Println(string(gitInitOutput))
+
+	f, err := os.Create(projectName + "/.gitignore")
+	if err != nil {
+		fmt.Println("skill issue", err)
+	}
+	f.WriteString("main\n*.exe")
 }
